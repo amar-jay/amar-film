66c29582296507872ff07c777cc5fecb1ebd6dca
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-04-09 07:18:37 +0300

    unused imports

--- src/api/request.js
+++ src/api/request.js
@@ -1,11 +1,11 @@
-// import { API_KEY } from "./env";
-// import 'dotenv/config' 
-const API_KEY = process.env.API_KEY
-const request = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`, 
-    fetchTopRated: `/movie/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
-    fetchActionMovies: `/trending/all/week?api_key=${API_KEY}&with_genres=20`
-}
-
-export default request;
+ import { API_KEY } from "./env";
+// import 'dotenv/config' 
+//const API_KEY = process.env.API_KEY
+const request = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`, 
+    fetchTopRated: `/movie/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
+    fetchActionMovies: `/trending/all/week?api_key=${API_KEY}&with_genres=20`
+}
+
+export default request;
