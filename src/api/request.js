f53177055ff9ccc871ff5ee724d14353862a6e58
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-04-09 06:37:36 +0300

    commit faults

--- src/api/request.js
+++ src/api/request.js
@@ -1,16 +1,11 @@
-import { API_KEY } from "./env";
+// import { API_KEY } from "./env";
 // import 'dotenv/config' 
-// const API_KEY = process.env.API_KEY
+const API_KEY = process.env.API_KEY
 const request = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`, 
-    // https://api.themoviedb.org/3/trending/all/week?api_key=b0ddba99e923a738f912b28e16561477
-    fetchTopRated: `/movie/all/week?api_key=${API_KEY}&language=en-US`, // Not working!!!
-    // https://api.themoviedb.org/3/movie/all/week?api_key=b0ddba99e923a738f912b28e16561477
+    fetchTopRated: `/movie/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
-    // https://api.themoviedb.org/3/discover/tv?api_key=b0ddba99e923a738f912b28e16561477&with_networks=123
-    fetchActionMovies: `/trending/all/week?api_key=${API_KEY}&with_genres=20`,
-    // https://api.themoviedb.org/3/trending/all/week?api_key=b0ddba99e923a738f912b28e16561477&with_genres=20
-
+    fetchActionMovies: `/trending/all/week?api_key=${API_KEY}&with_genres=20`
 }
 
 export default request;
